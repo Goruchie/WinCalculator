373082b86d142b87b35c1622c17de072f59a5015
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2024-07-26 19:10:26 -0300

    Implement try and catch solutions.

--- Form1.cs
+++ Form1.cs
@@ -20,10 +20,26 @@ namespace WinCalculator
         private void button1_Click(object sender, EventArgs e)
         {
             int a, b, c;
-            a = int.Parse(firstNumTb.Text);
-            b = int.Parse(secondNumTb.Text);
-            c = a + b;
-            resultLbl.Text = "= " + c;
+
+            try
+            {
+                a = int.Parse(firstNumTb.Text);
+                b = int.Parse(secondNumTb.Text);
+                c = a / b;
+                resultLbl.Text = "= " + c;
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Kindly, use only numbers to calculate...");
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("It's not possible to divide a number by zero...");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occurred, please report it to the provider...");
+            }
         }
     }
 }
